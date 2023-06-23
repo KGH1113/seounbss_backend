b4392e70f0eb967b49766c3196c173c98fbf1848
KGH1113 <gangguhyeon1113@gmail.com>
2023-06-23 13:19:20 +0900

    suggestion request backend added

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ app.use((req, res, next) => {
 // Middleware to parse JSON data
 app.use(bodyParser.json());
 
-// Object to store requested songs by date
-const requestedSongsByDate = {"Mon Jun 19 2023":[{"name":"김지오","studentNumber":"10508","songTitle":"오랜 날 오랜 밤","singer":"악뮤"},{"name":"구태윤","studentNumber":"11202","songTitle":"이브,프시케 그리고 부른 수염의 아내","singer":"르세라핌"},{"name":"이서현","studentNumber":"11223","songTitle":"Good As It Gets","singer":"이서현"},{"name":"이승준","studentNumber":"20719","songTitle":"가로수 그늘 아래에 서면","singer":"이승준"},{"name":"정재용","studentNumber":"10529","songTitle":"uptown funk","singer":"Bruno Mars"},{"name":"김세원 ","studentNumber":"10205","songTitle":"Broken Melodies ","singer":"NCT DREAM"},{"name":"","studentNumber":"","songTitle":"","singer":""},{"name":"천주원","studentNumber":"10530","songTitle":"손오공","singer":"세븐틴"}],"Tue Jun 20 2023":[{"name":"구태윤","studentNumber":"11202","songTitle":"작은 것들을 위한 시","singer":"방탄"},{"name":"이세은","studentNumber":"11023","songTitle":"척","singer":"마마무"}]};
+const requestedSongsByDate = {"Mon Jun 19 2023":[{"name":"김지오","studentNumber":"10508","songTitle":"오랜 날 오랜 밤","singer":"악뮤"},{"name":"구태윤","studentNumber":"11202","songTitle":"이브,프시케 그리고 부른 수염의 아내","singer":"르세라핌"},{"name":"이서현","studentNumber":"11223","songTitle":"Good As It Gets","singer":"이서현"},{"name":"이승준","studentNumber":"20719","songTitle":"가로수 그늘 아래에 서면","singer":"이승준"},{"name":"정재용","studentNumber":"10529","songTitle":"uptown funk","singer":"Bruno Mars"},{"name":"김세원 ","studentNumber":"10205","songTitle":"Broken Melodies ","singer":"NCT DREAM"},{"name":"","studentNumber":"","songTitle":"","singer":""},{"name":"천주원","studentNumber":"10530","songTitle":"손오공","singer":"세븐틴"}],"Tue Jun 20 2023":[{"name":"구태윤","studentNumber":"11202","songTitle":"작은 것들을 위한 시","singer":"방탄"},{"name":"이세은","studentNumber":"11023","songTitle":"척","singer":"마마무"}],"Wed Jun 21 2023":[{"name":"김리호","studentNumber":"10504","songTitle":"미안해","singer":"처리"},{"name":"구태윤","studentNumber":"11202","songTitle":"Hype boy","singer":"뉴진스"},{"name":"박우원","studentNumber":"20613","songTitle":"옛사랑","singer":"이문세"}],"Thu Jun 22 2023":[{"name":"강지후","studentNumber":"10601","songTitle":"썸 탈거야","singer":"볼빨간사춘기"},{"name":"test","studentNumber":"test","songTitle":"test","singer":"test"},{"name":"test1","studentNumber":"test1","songTitle":"test1","singer":"test1"},{"name":"test2","studentNumber":"test2","songTitle":"test2","singer":"test2"},{"name":"test3","studentNumber":"test3","songTitle":"test3","singer":"test3"},{"name":"test4","studentNumber":"test4","songTitle":"test4","singer":"test4"},{"name":"구태윤","studentNumber":"11202","songTitle":"불타오르네","singer":"BTS"}]}
+const requestedSuggestions = [];
 
-// Array to store the blacklist
 const blacklist = [];
+const suggestion_blacklist = [];
 
 // Function to check if a song request is valid
-function isRequestValid(name, studentNumber, songTitle, singer) {
+const isRequestValid = (name, studentNumber, songTitle, singer) => {
   const currentDate = new Date();
   const currentDateString = currentDate.toDateString();
 
@@ -67,7 +67,7 @@ function isRequestValid(name, studentNumber, songTitle, singer) {
 }
 
 
-// Function 1: Receive song requests
+// Function: Receive song requests
 app.post('/song-request', (req, res) => {
   const { name, studentNumber, songTitle, singer } = req.body;
 
@@ -91,6 +91,37 @@ app.post('/song-request', (req, res) => {
 
     res.status(200).send('노래가 성공적으로 신청되었습니다!');
   }
+});
+
+app.get('/view-request', (req, res) => {
+  const currentDate = new Date();
+  const currentDateString = currentDate.toDateString();
+
+  // Retrieve the requested songs for the current date
+  const requestedSongs = requestedSongsByDate[currentDateString] || [];
+
+  res.json(requestedSongs);
+});
+
+app.get('/view-all-requests', (req, res) => {
+  res.json(requestedSongsByDate);
+});
+
+app.post('/suggestion-request', (req, res) => {
+  const { name, studentNumber, suggestion } = req.body;
+
+  if (suggestion_blacklist.some(item => item.name === name || item.studentNumber === studentNumber)) {
+    res.status(400).json({ error:  '블랙리스트에 등록되신 것 같습니다.'});
+  } else {
+    // Add the requested song to the array for the current date
+    requestedSuggestions.push({ name, studentNumber, suggestion });
+
+    res.status(200).send('방송부 건의사항이 성공적으로 신청되었습니다!');
+  }
+});
+
+app.get('/view-suggestion', (req, res) => {
+  res.json(requestedSuggestions);
 });
 
 app.get('/', (req, res) => {
@@ -117,9 +148,9 @@ app.get('/', (req, res) => {
       </style>
     </head>
     <body>
-      <h1>Welcome to the Song Request Backend</h1>
+      <h1>Welcome to the SeounBSS Backend</h1>
       <p>
-        This backend server allows you to make song requests and view the requested songs.
+        This backend server allows you to make song requests, view the requested songs, request the suggestions and view the suggestions.
       </p>
       <p>To make a song request, send a POST request to the <div><code>/song-request</code></div> endpoint with the following JSON payload:</p><br>
       <div>
@@ -133,25 +164,24 @@ app.get('/', (req, res) => {
         </code>
       </div>
       <p>To view the requested songs, send a GET request to the <div><code>/view-request</code></div> endpoint.</p>
+      <br>
+      <br>
+      <br>
+      <p>To make a suggestion request, send a POST request to the <div><code>/suggestion-request</code></div> endpoint the following JSON payload:</p><br>
+      <div>
+        <code>
+          {<br>
+            &emsp;"name": "Your Name",<br>
+            &emsp;"studentNumber": "Your Student Number",<br>
+            &emsp;"suggestion": "Suggestion content"<br>
+          }
+        </code>
+      </div>
+      <p>To view the suggestion, send a GET request to the <div><code>/view-suggestion</code></div> endpoint.</p>
     </body>
     </html>    
     `
   );
-})
-
-// Function 2: Send requested songs as JSON
-app.get('/view-request', (req, res) => {
-  const currentDate = new Date();
-  const currentDateString = currentDate.toDateString();
-
-  // Retrieve the requested songs for the current date
-  const requestedSongs = requestedSongsByDate[currentDateString] || [];
-
-  res.json(requestedSongs);
-});
-
-app.get('/view-all-requests', (req, res) => {
-  res.json(requestedSongsByDate);
 });
 
 // Start the server
